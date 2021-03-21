22cd17d98dfa5e2a37e27c87cd8606e2beea0e73
Jack20215 <205281113@qq.com>
2021-03-21 12:36:24 +0800

    提交文件

---
+++ HelloGit/src/main/java/com/zhidao/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.zhidao.git;
+
+/**
+ * @Author: Zhangsl
+ * @Date: 2021/03/21 12:25
+ * @Description:
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
